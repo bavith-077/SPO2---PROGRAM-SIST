b88040acf3422526704e663cb5a4dad839818673
Bavith Mariyan <bavithmariyan12@gmail.com>
2024-11-29 11:56:46 +0530

    Add files via upload

---
+++ reverse1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class reverse1{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String reversed = "";
+        for(int i=str.length()-1;i>0;i--){
+            reversed += str.charAt(i);
+        }
+        System.out.println(reversed);
+    }
+}
